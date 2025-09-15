a708ab4b95e6a7efd2cf01ea232bb276da31ac9f
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2025-09-15 22:57:06 +0300

    add style for sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,10 +10,10 @@ export default function RootLayout({
     <html lang="en">
       <body className="min-h-screen m-0 p-0">
         <div className="flex h-screen">
-          <aside className="w-20 md:w-42 bg-gray-100">
+          <aside className="w-20 md:w-60 bg-slate-200">
             <Sidebar />
           </aside>
-          <main className="flex-1 p-6">{children}</main>
+          <main className="flex-1 p-6 bg-slate-50">{children}</main>
         </div>
       </body>
     </html>
