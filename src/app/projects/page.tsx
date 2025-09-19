3ecb0e84d0fa8e725a576653effee12e1cb2c503
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2025-09-19 10:48:54 +0300

    add projects page content

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,38 @@
+"use client";
+import countCharacters from "@/functions/countСharacters";
+import { useState } from "react";
+
 export default function ProjectsPage() {
-  return <h1>Statistics Page</h1>
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState<Record<string, number>>({});
+
+  const handleCount = () => {
+    setResult(countCharacters(input));
+  };
+
+  return (
+    <div className="space-y-4 w-96">
+      <h1 className="text-2xl font-bold">Project</h1>
+
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        className="w-full border rounded p-2"
+        placeholder="Введите текст"
+      />
+
+      <button
+        onClick={handleCount}
+        className="px-4 py-2 bg-blue-500 text-white rounded"
+      >
+        Count
+      </button>
+
+      <pre className="bg-gray-100 p-4 rounded">
+        {result && Object.keys(result).length > 0
+          ? JSON.stringify(result, null, 2)
+          : null}
+      </pre>
+    </div>
+  );
 }
