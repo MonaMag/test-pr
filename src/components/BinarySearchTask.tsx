2ccc99399b6f01faea4edeebc9ee96d98068b830
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2025-09-25 15:22:53 +0300

    fix

--- src/components/BinarySearchTask.tsx
+++ src/components/BinarySearchTask.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ProjectLayout from "./ProjectLayout";
+import binarySearch from "@/functions/binarySearch";
 
 const MAX_LENGTH = 20;
 
@@ -26,23 +27,6 @@ export function BinarySearchTask() {
     setArray(sortArray);
     setResult({});
   };
-
-  function binarySearch(arr: number[], num: number) {
-    let left = 0;
-    let right = array.length - 1;
-    let steps = 0;
-
-    while (left <= right) {
-      steps++;
-      const mid = Math.floor(left + right) / 2;
-
-      if (arr[mid] === num) return { index: mid, steps };
-      if (arr[mid] < num) left = mid + 1;
-      else right = mid - 1;
-    }
-
-    return { index: -1, steps };
-  }
 
   function handleSearch() {
     if (array.length === 0) {
