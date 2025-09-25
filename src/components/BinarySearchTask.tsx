18041ab935cae71097bce6f3e85c56e15bce73e6
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2025-09-25 15:20:37 +0300

    add binarySearch

--- src/components/BinarySearchTask.tsx
+++ src/components/BinarySearchTask.tsx
@@ -28,8 +28,20 @@ export function BinarySearchTask() {
   };
 
   function binarySearch(arr: number[], num: number) {
-    setResult({});
-    return { position: 2, step: 4 };
+    let left = 0;
+    let right = array.length - 1;
+    let steps = 0;
+
+    while (left <= right) {
+      steps++;
+      const mid = Math.floor(left + right) / 2;
+
+      if (arr[mid] === num) return { index: mid, steps };
+      if (arr[mid] < num) left = mid + 1;
+      else right = mid - 1;
+    }
+
+    return { index: -1, steps };
   }
 
   function handleSearch() {
